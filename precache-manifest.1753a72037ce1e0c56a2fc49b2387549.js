a13aefffd4211a27e5ab3aafc419152e9e84db45
Dr-Devine <98554800+Dr-Devine@users.noreply.github.com>
2022-06-26 18:44:40 +0100

    Updates

---
+++ precache-manifest.1753a72037ce1e0c56a2fc49b2387549.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "03f3044db23dec5c07da",
+    "url": "/Portfolio/static/css/main.4ebb0a78.chunk.css"
+  },
+  {
+    "revision": "03f3044db23dec5c07da",
+    "url": "/Portfolio/static/js/main.8347a415.chunk.js"
+  },
+  {
+    "revision": "29077a3a543c74022935",
+    "url": "/Portfolio/static/js/runtime~main.4fa906cf.js"
+  },
+  {
+    "revision": "b60dc569b45a105b2fbe",
+    "url": "/Portfolio/static/js/2.88e3c7de.chunk.js"
+  },
+  {
+    "revision": "2ded26446bc94b81347a",
+    "url": "/Portfolio/static/js/3.398cb210.chunk.js"
+  },
+  {
+    "revision": "f7ad3d72a01dd021d98d95d7aaabdbda",
+    "url": "/Portfolio/index.html"
+  }
+];
